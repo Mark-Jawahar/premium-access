3eb71260ae20d14d2c6da785e6db1855a5d3c49d
Mark-Jawahar <markjawaharj@gmail.com>
2026-02-21 16:30:45 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-// PASTE YOUR FIREBASE CONFIG HERE
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCLklB9xFWDWC3mvzNMPz9ADJ2P6eETyB4",
+  authDomain: "premium-page.firebaseapp.com",
+  projectId: "premium-page",
+  storageBucket: "premium-page.firebasestorage.app",
+  messagingSenderId: "35609923793",
+  appId: "1:35609923793:web:1d133a99d3a904cbec35f0"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
