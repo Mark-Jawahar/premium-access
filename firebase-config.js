c9f6def64a35a9370f16ed925d8d0b3fe3d60ab4
Mark-Jawahar <markjawaharj@gmail.com>
2026-02-21 16:58:47 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -16,42 +16,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// 4. Initialize reCAPTCHA
-window.initRecaptcha = () => {
-  if (!window.recaptchaVerifier) {
-    window.recaptchaVerifier = new RecaptchaVerifier(auth, 'recaptcha-container', {
-      'size': 'invisible',
-      'callback': (response) => {
-        // reCAPTCHA solved - you can log this or leave it empty
-        console.log("reCAPTCHA verified");
-      }
-    });
-  }
-};
-
-// 5. Send OTP Function
-window.sendOTP = async (phone) => {
-  try {
-    window.initRecaptcha();
-    const appVerifier = window.recaptchaVerifier;
-    
-    // Send the SMS
-    const confirmationResult = await signInWithPhoneNumber(auth, phone, appVerifier);
-    
-    // Store this result globally so you can access it when the user types in the code
-    window.confirmationResult = confirmationResult;
-    console.log("OTP sent successfully!");
-    
-    return confirmationResult;
-    
-  } catch (error) {
-    console.error("Error sending OTP: ", error);
-    
-    // It's a best practice to clear the reCAPTCHA if the SMS fails to send, 
-    // otherwise the user gets stuck if they try to click "Send" again.
-    if (window.recaptchaVerifier) {
-       window.recaptchaVerifier.clear();
-       window.recaptchaVerifier = null; 
-    }
-  }
-};
+// 4. EXPORT the tools so login.html can actually use them
+export { auth, RecaptchaVerifier, signInWithPhoneNumber };
